3c60166b20b2ea62b7239b62b586c850afd6f614
Richard <rico4live@gmail.com>
2023-06-29 18:12:41 +0100

    update to origin list

--- api/index.js
+++ api/index.js
@@ -25,7 +25,11 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+  origin: [
+    'http://127.0.0.1:5173', 
+    'http://localhost:5173', 
+    'https://airbnb-frontend-vgwj.onrender.com/'
+  ],
 }));
 
 cloudinary.config({
